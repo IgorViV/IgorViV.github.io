003be4183b510cc3a5834979b324493fbdec379e
IgorViV <iupgrader@mail.ru>
2019-01-23 16:33:15 +0300

    Добавляет файлы проекта Mishka

---
+++ Mishka/js/mobile-menu.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var navMain = document.querySelector('.page-menu');
+var navToggle = document.querySelector('.page-menu__button');
+
+if (navMain && navMain.classList.contains('page-menu--no-js')) {
+navMain.classList.remove('page-menu--no-js');
+navMain.classList.add('page-menu--close');
+}
+  navToggle.addEventListener('click', function() {
+    if (navMain.classList.contains('page-menu--close')) {
+      navMain.classList.remove('page-menu--close');
+      navMain.classList.add('page-menu--open');
+    } else {
+      navMain.classList.add('page-menu--close');
+      navMain.classList.remove('page-menu--open');
+    }
+  });
---
+++ Mishka/js/modal.js
@@ -0,0 +1,63 @@
+"use strict";
+
+var pageModal = document.querySelector('.page-modal');
+var modalSubmit = pageModal.querySelector('.page-modal__submit');
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+
+function onModalOpen () {
+  pageModal.classList.remove('page-modal--hidden');
+};
+
+function onModalClose () {
+  pageModal.classList.add('page-modal--hidden');
+};
+
+function isEscEvent (evt, action) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    action();
+  }
+};
+
+function isEnterEvent (evt, action) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    action();
+  }
+};
+
+if (document.querySelector('.promo__button-order')) {
+  var buttonOrder = document.querySelector('.promo__button-order');
+  buttonOrder.addEventListener('click', function () {
+    pageModal.classList.remove('page-modal--hidden');
+  });
+}
+
+if (document.querySelector('.catalog')) {
+  var catalog = document.querySelector('.catalog');
+  var catalogList = catalog.querySelector('.catalog__list');
+  var arrCatalogItems = catalogList.querySelectorAll('.catalog__list');
+
+  catalogList.addEventListener('click', function (evt) {
+    if (evt.target.tagName === 'BUTTON') {
+      if (evt.target.classList.contains('catalog__item-cart')) {
+        onModalOpen();
+      }
+    }
+  });
+}
+
+modalSubmit.addEventListener('click', function (evt) {
+  evt.preventDefault();
+
+  onModalClose();
+});
+
+modalSubmit.addEventListener('keydown', function (evt) {
+
+  isEnterEvent (evt, onModalClose);
+});
+
+document.addEventListener('keydown', function (evt) {
+
+  isEscEvent (evt, onModalClose);
+});
